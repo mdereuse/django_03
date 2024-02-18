5b3d241d8f3c6fb648dbc716c7250c009c7dc0af
Marie Dereuse <mdereuse@student.42.fr>
2024-02-18 22:59:48 +0100

    ex01 modified

--- ex01/my_script.sh
+++ ex01/my_script.sh
@@ -2,10 +2,10 @@
 VENV_DIRPATH="./local_lib"
 VENV_ACTIVATION_FILEPATH=$VENV_DIRPATH"/bin/activate"
 DEP_URL="https://github.com/jaraco/path.git"
-LOG_FILEPATH="venv_install.log"
+LOG_FILEPATH="local_lib_install.log"
 
 
-python3 -m pip --version
-python3 -m venv $VENV_DIRPATH
+virtualenv $VENV_DIRPATH --always-copy
 . $VENV_ACTIVATION_FILEPATH
+python3 -m pip --version
 python3 -m pip install --log $LOG_FILEPATH --force-reinstall git+$DEP_URL && python3 my_program.py
