d9bfed2b4a150ac334070e0d7a8d9919c1c63b44
Marie Dereuse <mdereuse@student.42.fr>
2024-02-18 23:57:20 +0100

    ex04 modified

--- ex04/my_script.sh
+++ ex04/my_script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 BASH_FILEPATH="/bin/bash"
-PYTHON_FILEPATH="/usr/bin/python3"
 VENV_DIRPATH="django_venv"
 VENV_ACTIVATION_FILEPATH=$VENV_DIRPATH"/bin/activate"
 
-$PYTHON_FILEPATH -m venv $VENV_DIRPATH
+virtualenv $VENV_DIRPATH --always-copy
 . $VENV_ACTIVATION_FILEPATH
 python3 -m pip install -r requirement.txt
 $BASH_FILEPATH -c ". $VENV_ACTIVATION_FILEPATH; exec $BASH_FILEPATH -i"
